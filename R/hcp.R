7657c600845765a47a7f968ecb52719927aa4718
mvaniterson <mviterson@gmail.com>
2015-03-25 13:26:30 +0100

    modified hcp interface

---
+++ R/hcp.R
@@ -0,0 +1,94 @@
+##' R/Rcpp implementation of the HCP algorithm
+##'
+##' Objective:
+##' This function solves the following problem:
+##' argmin_{Z,B,U}   ||Y-Z*B||_2 + lambda1*||Z-F*U||_2 + lambda2*||B||_2 +
+##' lambda_3||U||_2
+##'
+##' To use the residual data: Residual = Y - Z*B
+##'
+##' Note: k>0, lambda1>0, lambda2>0, lambda3>0 must be set by the user based on
+##' the data at hand. one can set these values using cross-validation, by
+##' evaluating the "performance" of the  resulting residual data on a desired
+##' task. typically, if lambda>5, then hidden factors match the known covariates closely.
+##' @title R/Rcpp implementation of the HCP algorithm
+##' @param F a matrix nxd of known covariates, where n is the number of
+##' subjects and d is the number of known covariates. *must be standardize
+##' (columns have 0 mean and constant SS).
+##' @param Y a matrix of nxg of expression data (must be standardized (columns
+##' scaled to have constant SS and mean 0). ** use standardize function to standardize F and Y.
+##' @param k  number of inferred hidden components (k is an integer)
+##' @param lambda1 model parameter 1
+##' @param lambda2 model parameter 2
+##' @param lambda3 model parameter 3
+##' @param iter (optional) iter: number of iterations (default = 100);
+##' @param stand default standardize data TRUE
+##' @param log default log-transformation TRUE
+##' @param fast default use fast RcppArmadillo implementation
+##' @param verbose default TRUE
+##' @return list
+##' Z: matrix of hidden components, dimensionality: nxk,
+##' B: matrix of effects of hidden components, dimensionality: kxg,
+##' o: value of objective function on consecutive iterations.
+##' @author mvaniterson
+##' @references \url{http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0068141}
+##' @examples
+##' data(hcpp)
+##' F <- data$F
+##' Y <- data$Y
+##' k <- 10
+##' lambda1 <- 20
+##' lambda2 <- 1
+##' lambda3 <- 1
+##' iter <- 100
+##' ##and run
+##' ##Rres <- hcp(Y, F, k, lambda1, lambda2, lambda3, iter)
+hcp <- function(F, Y, k, lambda1, lambda2, lambda3, iter=NULL, stand=TRUE, log=TRUE, fast=TRUE, verbose=TRUE)
+  {
+    t0 <- proc.time()
+    ## (1) take log of read counts
+    if(log)
+      {
+        message("Use log-data transformation.")
+        Y <- log2(2+Y)
+      }
+
+    if(sum(is.na(F)) > 0 | sum(is.na(Y)) > 0)
+      stop("1: NA's in input data are not allowed!")
+
+    standardize <- function(x)
+      {
+        x <- apply(x, 1, function(x) x - mean(x))
+        x <- apply(x, 1, function(x) x/sqrt(sum(x^2)))
+        x
+      }
+
+    ## (2) standardize the data
+    if(stand) {
+      Yn <- scale(Y, scale=apply(Y, 2, function(x) sqrt(sum((x - mean(x))^2))))
+      Fn <- scale(F, scale=apply(F, 2, function(x) sqrt(sum((x - mean(x))^2))))
+    }
+    else
+      {
+        Yn <- Y
+        Fn <- F
+      }
+
+    if(sum(is.na(Fn)) > 0 | sum(is.na(Yn)) > 0)
+      stop("2: NA's in input data are not allowed!")
+
+    ## (3) HCP
+    if(fast)
+      res <- rcpparma_hcp(Fn, Yn, k, lambda1, lambda2, lambda3, iter)
+    else
+      res <- r_hcp(Fn, Yn, k, lambda1, lambda2, lambda3, iter)
+
+    if(verbose)
+      message(paste("Finished after", res$iter, "iterations of", iter, "iterations."))
+
+    Z <- res$Z
+    B <- res$B
+
+    message(paste("The batch correction took:", round((proc.time() - t0)[3], 2), "seconds."))
+    return(list(Res = t(Yn - Z%*%B), Cov = Z))
+  }
--- R/r_hcp.R
+++ R/r_hcp.R
@@ -1,46 +1,6 @@
-##' pure R implementation of the HCP algorithm
-##'
-##' Objective:
-##' This function solves the following problem:
-##' argmin_{Z,B,U}   ||Y-Z*B||_2 + lambda1*||Z-F*U||_2 + lambda2*||B||_2 +
-##' lambda_3||U||_2
-##'
-##' To use the residual data: Residual = Y - Z*B
-##' 
-##' Note: k>0, lambda1>0, lambda2>0, lambda3>0 must be set by the user based on
-##' the data at hand. one can set these values using cross-validation, by
-##' evaluating the "performance" of the  resulting residual data on a desired
-##' task. typically, if lambda>5, then hidden factors match the known covariates closely.
-##' @title pure R implementation of the HCP algorithm
-##' @param F a matrix nxd of known covariates, where n is the number of
-##' subjects and d is the number of known covariates. *must be standardize
-##' (columns have 0 mean and constant SS).
-##' @param Y a matrix of nxg of expression data (must be standardized (columns
-##' scaled to have constant SS and mean 0). ** use standardize function to standardize F and Y. 
-##' @param k  number of inferred hidden components (k is an integer)
-##' @param lambda1 model parameter 1
-##' @param lambda2 model parameter 2
-##' @param lambda3 model parameter 3
-##' @param iter (optional) iter: number of iterations (default = 100);
-##' @return list
-##' Z: matrix of hidden components, dimensionality: nxk,
-##' B: matrix of effects of hidden components, dimensionality: kxg,
-##' o: value of objective function on consecutive iterations.
-##' @author mvaniterson
-##' @references \url{http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0068141}
-##' @examples
-##' data(hcp)
-##' F <- data$F
-##' Y <- data$Y
-##' k <- 10
-##' lambda1 <- 20
-##' lambda2 <- 1
-##' lambda3 <- 1
-##' iter <- 100
-##' Rres <- r_hcp(Y, F, k, lambda1, lambda2, lambda3, iter)
 r_hcp <- function(F, Y, k, lambda1, lambda2, lambda3, iter=NULL)
-  {    
-  
+  {
+
     if(is.null(iter))
       iter <- 100
 
@@ -70,7 +30,7 @@ r_hcp <- function(F, Y, k, lambda1, lambda2, lambda3, iter=NULL)
     diagB <- diag(nrow(B))
     diagZ <- diag(ncol(Z))
     diagU <- diag(nrow(U))
-    
+
     if(iter > 0)
       {
         o <- numeric(iter)
@@ -81,7 +41,7 @@ r_hcp <- function(F, Y, k, lambda1, lambda2, lambda3, iter=NULL)
 
             ##Z <- (Y%*%t(B) + lambda1*F%*%U) %*% solve(B%*%t(B) + lambda1*diagB)
             Z <- (tcrossprod(Y, B) + lambda1*F%*%U) %*% solve(tcrossprod(B) + lambda1*diagB)
-            
+
             ##B <- solve(t(Z)%*%Z + lambda2*diagZ, t(Z)%*%Y)
             B <- solve(crossprod(Z) + lambda2*diagZ, crossprod(Z,Y))
 
