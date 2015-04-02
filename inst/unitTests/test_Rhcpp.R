d18a0d58fc46fe31b79de72d9d277be996175a8c
mvaniterson <mviterson@gmail.com>
2015-04-02 10:35:33 +0200

    return residuals samples by features

--- inst/unitTests/test_Rhcpp.R
+++ inst/unitTests/test_Rhcpp.R
@@ -30,5 +30,17 @@ lambdaRange <- c(1,5)
 kRange <- c(10)
 res <- hcpcv(F, Y, kRange, lambdaRange, iter, performance=function(res) {sum(res$Res)})
 res
+dim(Y)
 
+n <- 2000
+q <- 20
+p <- 300000
+F <- matrix(runif(n*q), n, q)
+Y <- matrix(runif(n*p), n, p)
+##res <- hcp(F, Y, k, lambda1, lambda2, lambda3, iter)
 
+(p+q)/n / (98/2)
+
+(100*(p+q)/n * 0.2044898)/60 ##worse performance  100 iterations
+
+0.2044898 sec per iteration per features/per samples
