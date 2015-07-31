61636689033c31ff0dc2c0f95d59f98718820c63
mvaniterson <mviterson@gmail.com>
2015-07-31 14:12:54 +0200

    fix typo

--- R/r_hccpp.R
+++ R/r_hccpp.R
@@ -19,16 +19,16 @@ r_hcpp <- function(Z, Y, x, k, lambda1, lambda2, lambda3, iter=100) {
     ##predefine 
     diagB <- diagW <- diag(k)    
     diagA <- diag(nrow(A))
-    xtx <- as.numeric(crossprod(x)) ##since this is a scalar
+    xtxinvxt <- t(x)/as.numeric(crossprod(x))
     
     if(iter > 0) {
         o <- numeric(iter)
         for(ii in 1:iter) {
-            o[ii] <- norm(Y-W%*%B, type="F")^2 + lambda1*norm(W-Z%*%A, type="F")^2 + lambda2*norm(B, type="F")^2 + lambda3*norm(A, type="F")^2            
+            o[ii] <- norm(Y-x%*%g- W%*%B, type="F")^2 + lambda1*norm(W-Z%*%A, type="F")^2 + lambda2*norm(B, type="F")^2 + lambda3*norm(A, type="F")^2            
             W <- (tcrossprod(Y-x%*%g, B) + lambda1*Z%*%A)%*%solve(tcrossprod(B) + lambda1*diagB)
             B <- solve(crossprod(W) + lambda2*diagW, crossprod(W,Y-x%*%g))         
-            A <- solve(crossprod(F) + (lambda3/lambda1)*diagA, crossprod(F,Z))                       
-            g <- t(x)%*%(Y - Z%*%B)/xtx
+            A <- solve(crossprod(Z) + (lambda3/lambda1)*diagA, crossprod(Z,W))                       
+            g <- xtxinvxt%*%(Y - W%*%B)/xtx
             
             if(ii > 1) {
                 if((abs(o[ii] - o[ii-1])/o[ii]) < tol)
