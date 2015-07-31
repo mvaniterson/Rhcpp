83e28ea22a4c665b68aac6b29d5520179ead87ad
mvaniterson <mviterson@gmail.com>
2015-07-31 14:06:26 +0200

    fix typo

--- R/r_hccpp.R
+++ R/r_hccpp.R
@@ -25,8 +25,8 @@ r_hcpp <- function(Z, Y, x, k, lambda1, lambda2, lambda3, iter=100) {
         o <- numeric(iter)
         for(ii in 1:iter) {
             o[ii] <- norm(Y-W%*%B, type="F")^2 + lambda1*norm(W-Z%*%A, type="F")^2 + lambda2*norm(B, type="F")^2 + lambda3*norm(A, type="F")^2            
-            W <- (tcrossprod(Y-x%*%gamma, B) + lambda1*Z%*%A)%*%solve(tcrossprod(B) + lambda1*diagB)
-            B <- solve(crossprod(W) + lambda2*diagW, crossprod(W,Y-x%*%gamma))         
+            W <- (tcrossprod(Y-x%*%g, B) + lambda1*Z%*%A)%*%solve(tcrossprod(B) + lambda1*diagB)
+            B <- solve(crossprod(W) + lambda2*diagW, crossprod(W,Y-x%*%g))         
             A <- solve(crossprod(F) + (lambda3/lambda1)*diagA, crossprod(F,Z))                       
             g <- t(x)%*%(Y - Z%*%B)/xtx
             
